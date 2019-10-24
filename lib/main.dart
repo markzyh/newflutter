3e983129216249869951217ce5b8baf8c3345e43
Markzyh <980511383@qq.com>
2019-10-24 17:44:58 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,96 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = new Container(
-      padding: const EdgeInsets.all(32.0),
-      child: new Row(
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          new Expanded(
-            child: new Column(
+          Expanded(
+            /*1*/
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                new Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: new Text(
-                    'Oeschinen Lake Campground',
-                    style: new TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text("test title",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
                 ),
-                new Text(
-                  'Kandersteg, Switzerland',
-                  style: new TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
+                Text('test content', style: TextStyle(color: Colors.grey[500]))
               ],
             ),
           ),
-          new Icon(
+          /*3*/
+          Icon(
             Icons.star,
             color: Colors.red[500],
           ),
-          new Text('41'),
+          Text('41')
         ],
       ),
     );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, "CALL"),
+          _buildButtonColumn(color, Icons.near_me, "ROUTE"),
+          _buildButtonColumn(color, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+          'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest',
+          softWrap: true),
+    );
+
+    Widget imageSection = Image.asset(
+      'images/lake.jpg',
+      width: 600,
+      height: 240,
+      fit: BoxFit.cover,
+    );
+
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: Column(
-          children: [
-            titleSection,
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('flutter layout demo'),
+          ),
+          body: Column(
+            children: [imageSection, titleSection, buttonSection, textSection],
+          )),
+    );
+  }
+
+  Widget _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ))
+      ],
     );
   }
 }

f00f1a94a3da8b6bd16f988a04b4979b3c2ba7d1
Markzyh <980511383@qq.com>
2019-10-24 18:27:59 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,12 @@ class MyApp extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41')
+          // Icon(
+          //   Icons.star,
+          //   color: Colors.red[500],
+          // ),
+          FavoriteWidget(),
+          // Text('48')
         ],
       ),
     );
@@ -90,6 +91,48 @@ class MyApp extends StatelessWidget {
                 color: color,
               ),
             ))
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(child: Text('$_favoriteCount')),
+        )
       ],
     );
   }
